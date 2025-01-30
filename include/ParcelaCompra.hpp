817a6dc1c539ea97967c60730a72a8e8068cd7d8
LucasPimentaB <lucaspimentabraga@gmail.com>
2025-01-30 19:42:44 -0300

    fix: corrigir e refazer a classe ParcelaCompra

---
+++ include/ParcelaCompra.hpp
@@ -0,0 +1,15 @@
+#ifndef PARCELACOMPRA_HPP
+#define PARCELACOMPRA_HPP
+
+#include "Compra.hpp"
+#include <vector>
+#include <string>
+#include "Operacoes.hpp"
+
+class ParcelaCompra
+{
+public:
+    static void ParcelarCompra(float valor, const std::string& categoriaEscolhida, const std::string& data, int numParcelas, Operacoes& operacoes);
+};
+
+#endif
